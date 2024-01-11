c95665dd2727e02d3525757281ca948f99b22d93
Kutlwano-Ncamani <156326603+Kutlwano-Ncamani@users.noreply.github.com>
2024-01-11 18:52:26 +0200

    Add files via upload

---
+++ PRG261_Milestone/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PRG_Milestone
+{
+    internal class Program
+    {
+        /*
+         Kutlwano Ncamani 577489
+         Zukhanye Lithemba Mavuso 577851
+         Ayanda Shanelle Vungwana 577874
+         */
+        static void Main(string[] args)
+        {
+            string choice;
+            // Create a student management system
+            StudentManagementSystem managementSystem = new StudentManagementSystem();
+            Console.WriteLine("Welcome To Belgium Campus (Stellenbosch Campus) Registration");
+            do
+            {
+                // Ask the user to input values for a new student
+                Console.WriteLine("Please enter your Student ID");
+                int studentID = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter your Name");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Please enter your Surname");
+                string surname = Console.ReadLine();
+
+                Console.WriteLine("Gender (Please enter 1 for Male or 2 for Female)");
+                Gender gender = (Gender)int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter your email");
+                string email = Console.ReadLine();
+
+                Console.WriteLine("Please enter your date of birth in this format (yyyy-mm-dd)");
+                DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter your Phone number ");
+                string phoneNumber = Console.ReadLine();
+
+                Console.WriteLine("Qualification (Please enter 1 for Degree or 2 for Diploma)");
+                Qualification qualification = (Qualification)int.Parse(Console.ReadLine());
+
+                // Create a new student object with the input values
+                Student student = new Student(studentID, name, surname, gender, email, dateOfBirth, phoneNumber, qualification);
+
+
+                // Register some sample students
+                managementSystem.RegisterStudent(new Student(studentID, name, surname, gender, email, dateOfBirth, phoneNumber, qualification));
+
+
+                Console.WriteLine("Would you like to add another student? (y/n)");
+                choice = Console.ReadLine();
+                Console.Clear();
+            } while (choice == "y");
+
+            Console.WriteLine("This is a list of all the registered students");
+            Console.WriteLine("---------------------------------------------");
+            managementSystem.DisplayRegisteredStudents();
+            Console.WriteLine("------------PRESS ANY KEY TO EXIT------------");
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ PRG261_Milestone/Student.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG_Milestone
+{
+    // Enum to represent student gender
+    enum Gender
+    {
+        Male =1,
+        Female
+    }
+
+    // Enum to represent student qualification
+    enum Qualification
+    {
+        Degree =1 ,
+        Diploma
+    }
+
+    // Class to represent a student
+    class Student
+    {
+        public int StudentID;
+        public string Name;
+        public string Surname;
+        public Gender Gender;
+        public string Email;
+        public DateTime DateOfBirth;
+        public string PhoneNumber;
+        public Qualification Qualification;
+        public decimal Discount;
+
+        public Student(int studentID, string name, string surname, Gender gender, string email, DateTime dateOfBirth, string phoneNumber, Qualification qualification)
+        {
+            StudentID = studentID;
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Email = email;
+            DateOfBirth = dateOfBirth;
+            PhoneNumber = phoneNumber;
+            Qualification = qualification;
+            Discount = CalculateDiscount();
+        }
+
+        // Calculate the discount based on student's age and gender
+        private decimal CalculateDiscount()
+        {
+            decimal discount = 0;
+
+            int age = DateTime.Now.Year - DateOfBirth.Year;
+
+            if (age > 25)
+            {
+                if (Gender == Gender.Female)
+                    discount = 0.3m;
+                else if (Gender == Gender.Male)
+                    discount = 0.19m;
+            }
+
+            return discount;
+        }
+
+        // Calculate the total fees after applying the discount
+        public decimal CalculateTotalFees()
+        {
+            decimal baseFee = (Qualification == Qualification.Degree) ? 85000 : 45000;
+            decimal discountedFee = baseFee - (baseFee * Discount);
+            return discountedFee;
+        }
+    }
+
+    // Class to manage the student registration system
+    class StudentManagementSystem
+    {
+        private List<Student> students;
+
+        public StudentManagementSystem()
+        {
+            students = new List<Student>();
+        }
+
+        // Register a new student
+        public void RegisterStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        // Display details of all registered students
+        public void DisplayRegisteredStudents()
+        {
+            foreach (var student in students)
+            {
+                Console.WriteLine("Student ID: " + student.StudentID);
+                Console.WriteLine("Name: " + student.Name + " " + student.Surname);
+                Console.WriteLine("Gender: " + student.Gender);
+                Console.WriteLine("Email: " + student.Email);
+                Console.WriteLine("Date of Birth: " + student.DateOfBirth.ToShortDateString());
+                Console.WriteLine("Phone Number: " + student.PhoneNumber);
+                Console.WriteLine("Qualification: " + student.Qualification);
+                Console.WriteLine("Discount: " + (student.Discount * 100) + "%");
+                Console.WriteLine("Total Fees: R" + student.CalculateTotalFees());
+                Console.WriteLine();
+            }
+        }
+    }
+}
